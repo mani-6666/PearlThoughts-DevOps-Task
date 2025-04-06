8b5b5d342b8d41191d149ceff67faaa05a0f8990
mani-6666 <nvvdmanikanta6666@gmail.com>
2025-04-06 11:49:29 +0530

    Terraform added

---
+++ Terraform/main.tf
@@ -0,0 +1,116 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "medusa_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "medusa-vpc"
+  }
+}
+
+resource "aws_subnet" "medusa_subnet" {
+  vpc_id     = aws_vpc.medusa_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "medusa-subnet"
+  }
+}
+
+resource "aws_security_group" "medusa_sg" {
+  vpc_id = aws_vpc.medusa_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "medusa-sg"
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "ecs-task-execution-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role     = aws_iam_role.ecs_task_execution_role.name
+}
+
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa_cluster"
+
+  tags = {
+    Name = "medusa-cluster"
+  }
+}
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa_task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = <<DEFINITION
+[
+  {
+    "name": "medusa",
+    "image": "medusajs/medusa:latest",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa_service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.medusa_subnet.id]
+    security_groups = [aws_security_group.medusa_sg.id]
+  }
+
+  launch_type = "FARGATE"
+}
---
+++ Terraform/terraform.tf
@@ -0,0 +1,2 @@
+aws_access_key = "AKIAT7JJVC6EL3H4F3S"
+aws_secret_key = "St36/vLK7oxPDTEtN8yQ8lTQvmijjyWX4cl/hme"
